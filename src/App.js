4cb242215350f0fc3ca286c19b0051c74b32ab57
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-25 22:13:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Container } from 'react-bootstrap';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+import Header from './compnents/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Container>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/services" element={<Services/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </Router>
+      </Container>
     </div>
   );
 }
