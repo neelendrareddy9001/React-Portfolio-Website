3da0afb6af509a3385f8eca04eb04f07800bbaeb
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-25 22:36:09 +0100

    Update Projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import project1 from '../images/project1.jpg';
+import project2 from '../images/project2.jpg';
 
 const Projects = () => {
+  const ProjectList = [{
+        id : 1,
+        name : "Marketing site design and build",
+        imageUrl : project1,
+    },
+    {
+        id : 2,
+        name : "Marketing site design and build",
+        imageUrl : project2,
+    },
+    {
+        id : 3,
+        name : "Marketing site design and build",
+        imageUrl : project1,
+    },
+    {
+        id : 4,
+        name : "Marketing site design and build",
+        imageUrl : project2
+    },
+    {
+        id : 5,
+        name : "Marketing site design and build",
+        imageUrl : project1
+    },
+    {
+        id : 6,
+        name : "Marketing site design and build",
+        imageUrl : project2
+    }
+    ]
   return (
-    <div>Projects</div>
+      <div className='projects'>
+        <h2>Projects</h2>
+        <div className='wrapper'>
+            {ProjectList.map((item) =>{
+                return <div>
+                    <Card>
+                        <Card.Img variant='top' src={item.imageUrl}/>
+                        <Card.Body>
+                            <Card.Title>{item.name}</Card.Title>
+                        </Card.Body>
+                    </Card>
+                </div>
+            })}
+        </div>
+    </div>
   )
 }
 
