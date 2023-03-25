dd1e6d623cc690e18826670cf05935bf63871604
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-25 23:01:39 +0100

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,67 @@
-import React from 'react'
+import React from 'react';
+import { Button, Col, Form, FormControl, FormGroup, FormLabel, ListGroup, ListGroupItem, Row } from 'react-bootstrap';
 
 const Contact = () => {
+    const handleSubmit = () => {};
   return (
-    <div>Contact</div>
+    <div className='contact'>
+        <h2>Contact</h2>
+        <Row>
+            <Col>
+                <div className='contact-form'>
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup className='mb-3' controlId='validationCustom01'>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl required type="text" placeholder='Name'/>
+                    </FormGroup>
+                    <FormGroup className='mb-3' controlId='validationCustom02'>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl required type="email" placeholder='Email'/>
+                    </FormGroup>
+                    <FormGroup className='mb-3' controlId='validationCustom03'>
+                        <FormLabel>Message</FormLabel>
+                        <FormControl required as="textarea" placeholder='message'/>
+                    </FormGroup>
+                    <Button type="submit">Submit Form</Button>
+                </Form>
+                </div>
+            </Col>
+            <Col>
+                <div className='contact-data'>
+                    <p>
+                        If you want to chat about a project — email me on
+                        dipesh@malvia.com.
+                    </p>
+                    <p>
+                        I can help designing a website, designing a new product, improving
+                        an existing part of your product, building a strong design system,
+                        building websites in Webflow, or designing a custom icon set for
+                        your business.{" "}
+                    </p>
+                    <p>
+                        Currently based in Wroclaw, Poland — available for remote-friendly
+                        work from December 2022.
+                    </p>
+                    <p>
+                        <ListGroup horizontal>
+                            <ListGroupItem>
+                                <a href="#">YouTube</a>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <a href="#">Linkedin</a>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <a href="#">Instagram</a>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <a href="#">Twitter</a>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </p>
+                </div>
+            </Col>
+        </Row>
+    </div>
   )
 }
 
