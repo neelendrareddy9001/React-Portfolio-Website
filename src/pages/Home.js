75a22efdd55ccf2e20efcd49e3b9dab290d11603
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-25 22:19:24 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+        <div className='name'>
+            Neelendrareddy<font>Panditi</font>
+        </div>
+        <div className='desc'>
+            <font>FullStack Developer</font> and <font>Mern Developer</font> based in Poland
+        </div>
+    </div>
   )
 }
 
