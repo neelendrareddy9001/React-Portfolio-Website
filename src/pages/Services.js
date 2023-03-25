d20c8df7405b8bd017faf55d086abe1297afcb96
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-25 22:45:25 +0100

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import project1 from '../images/project1.jpg';
+import project2 from '../images/project2.jpg';
+
 
 const Services = () => {
+  const ServiceList = [{
+        id : 1,
+        name : "Marketing site design and build",
+        imageUrl : project1,
+    },
+    {
+        id : 2,
+        name : "Marketing site design and build",
+        imageUrl : project2,
+    },
+    {
+        id : 3,
+        name : "Marketing site design and build",
+        imageUrl : project1,
+    }
+    ]
   return (
-    <div>Services</div>
+     <div className='services'>
+        <h2>Services</h2>
+        <div className='wrapper'>
+            {ServiceList.map((item) => {
+                return <div>
+                    <Card>
+                        <Card.Img variant='top' src={item.imageUrl}/>
+                        <Card.Body>
+                            <Card.Title>
+                                {item.name}
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                </div>
+            })}
+        </div>
+    </div>
   )
 }
 
