3a73f4b56a8b49e1e4fa3b5fe78668f6f134b957
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-03-26 22:13:35 +0200

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,45 +1,64 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import project1 from '../images/project1.jpg';
-import project2 from '../images/project2.jpg';
-
+import React from "react";
+import { Card } from "react-bootstrap";
+import { motion } from "framer-motion";
+import project1 from "../images/project1.jpg";
+import project2 from "../images/project2.jpg";
 
 const Services = () => {
-  const ServiceList = [{
-        id : 1,
-        name : "Marketing site design and build",
-        imageUrl : project1,
+  const serviceList = [
+    {
+      id: 1,
+      name: "Consulting",
+      imageUrl: project2,
     },
     {
-        id : 2,
-        name : "Marketing site design and build",
-        imageUrl : project2,
+      id: 2,
+      name: "Web Application Development",
+      imageUrl: project1,
     },
     {
-        id : 3,
-        name : "Marketing site design and build",
-        imageUrl : project1,
-    }
-    ]
-  return (
-     <div className='services'>
-        <h2>Services</h2>
-        <div className='wrapper'>
-            {ServiceList.map((item) => {
-                return <div>
-                    <Card>
-                        <Card.Img variant='top' src={item.imageUrl}/>
-                        <Card.Body>
-                            <Card.Title>
-                                {item.name}
-                            </Card.Title>
-                        </Card.Body>
-                    </Card>
-                </div>
-            })}
-        </div>
-    </div>
-  )
-}
+      id: 3,
+      name: "Products Support",
+      imageUrl: project2,
+    },
+  ];
 
-export default Services
+  return (
+    <motion.div
+      initial={{ scaleY: 0 }}
+      animate={{ scaleY: 1 }}
+      exit={{ scaleY: 0 }}
+    >
+      <div className="services">
+        <h2>Services</h2>
+        <div className="wrapper">
+          {serviceList.map((item) => {
+            return (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  transition: { duration: 1, ease: "easeInOut" },
+                }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: { duration: 0.5 },
+                }}
+              >
+                <Card>
+                  <Card.Img variant="top" src={item.imageUrl} />
+                  <Card.Body>
+                    <Card.Title>{item.name}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </motion.div>
+            );
+          })}
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Services;
